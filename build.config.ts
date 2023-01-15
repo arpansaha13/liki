014dda8ec875f39d4d57ef889ec9849dbc1e8244
arpansaha13 <arpansaha1300@gmail.com>
2023-01-15 11:46:23 +0530

    chore: add alias to 'src' directory

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,12 @@
+import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   rollup: {
     inlineDependencies: true,
+  },
+  alias: {
+    '~': resolve(process.cwd(), 'src'),
   },
   entries: ['src/cli'],
   externals: ['node:url', 'node:path', 'node:child_process', 'node:process', 'node:os'],
