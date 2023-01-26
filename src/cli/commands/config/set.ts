22927a0d7dec96eaf4ac1174ba7fa0b91bd71399
arpansaha13 <arpansaha1300@gmail.com>
2023-01-26 20:59:08 +0530

    feat(config-set): validate new values before updating

--- src/cli/commands/config/set.ts
+++ src/cli/commands/config/set.ts
@@ -1,9 +1,13 @@
 import mri from 'mri'
 import { homedir } from 'node:os'
 import { resolve } from 'node:path'
+import { existsSync } from 'node:fs'
 
+import createLogger from '~/logger'
 import { writeJsonFile } from '~/utils/base'
 import { getGlobalConfig, type LikiConfigType } from '~/utils/config'
+
+const logger = createLogger()
 
 /**
  * ### set &lt;key&gt; &lt;value&gt;
@@ -20,10 +24,32 @@ export default async function setConfig() {
   const globalConfigOptions = new Set<keyof LikiConfigType>(['pkgManager', 'storeDir'])
 
   // args._[0] is `config` and args._[1] is `set`
-  const [, , key, value] = args._ // TODO: add validation for new values
+  const [, , key, value] = args._
+
+  const isValid = validateOption(key as keyof LikiConfigType, value)
+  if (!isValid) process.exit(1)
 
   if (globalConfigOptions.has(key as keyof LikiConfigType)) {
     globalConfig[key as keyof LikiConfigType] = value as any
   }
   await writeJsonFile(globalConfigPath, globalConfig)
+}
+
+/** Validate the key-value pair. */
+function validateOption(key: keyof LikiConfigType, value: any): boolean {
+  switch (key) {
+    case 'pkgManager':
+      if (!['npm', 'yarn', 'pnpm'].includes(value)) {
+        logger.error(`Invalid value "${value}" for "${key}".`)
+        return false
+      }
+      break
+    case 'storeDir':
+      if (!existsSync(resolve(value))) {
+        logger.error(`The given path "${value}" could not be resolved.`)
+        return false
+      }
+      break
+  }
+  return true
 }
